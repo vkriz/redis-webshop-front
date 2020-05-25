2fc85e796559c65166d4f426c39e17d05c9a334c
Valentina Križ <valentina.vkriz@gmail.com>
2020-05-25 16:00:49 +0200

    best sellers title added

--- src/views/BestSellers.vue
+++ src/views/BestSellers.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="bestsellers">
     <Navbar/>
+    <div class="container">
+      <h2 class="mb-4">Best selling items</h2>
+    </div>
+    
     <Products :productList="productList"/>
   </div>
 </template>
